814089d03112c380a4ced9cff8c0684831ae2e20
Maksym Triteinikov <i.max.trit@gmail.com>
2023-05-05 23:45:31 +0300

    some bags fixed

--- src/index.js
+++ src/index.js
@@ -14,42 +14,48 @@ const gallery = document.querySelector(".gallery");
 const loadMore = document.querySelector(".load-more");
 
 searchForm.addEventListener("submit", onSearch);
-
 // loadMore.addEventListener("click", loadMoreImages);
 loadMore.style.display = "none";
+
+// Інфініті скролл. Ініціалізація
 const observer = new IntersectionObserver(onIntersection, { threshold: 0.5 });
 
+// Лайтбокс. Ініціалізація
 const lightbox = new simpleLightbox('.gallery a');
 
 let page = 1;
 let limit = 40;
 let searchQuery = "";
 
+
 // Стартовий пошук========================
 async function onSearch(e) {
   e.preventDefault();
-
-  const inputValue = e.currentTarget.elements.searchQuery.value.trim().toLowerCase();
+  const inputValue = e.currentTarget.elements.searchQuery.value.trim().toLowerCase();  
 
   if (searchQuery !== inputValue) {
     gallery.innerHTML = "";
-    page = 1
+    page = 1;
     // loadMore.style.display = "none";
-  }
+  };
+
+  if (!inputValue) {
+    return Notify.info("Please enter some value!");
+  };
+
   searchQuery = inputValue;
   try {
     const response = await getImages();
   if (!response.totalHits) {
     return Notify.failure("Sorry, there are no images matching your search query. Please try again.")    
   }
-  Notify.success(`Hooray! We found ${response.totalHits} images.`);
-  
-  createMarkupGallery(response.hits);  
+    Notify.success(`Hooray! We found ${response.totalHits} images.`);  
+    createMarkupGallery(response.hits);    
     page += 1;
     observer.observe(gallery.lastElementChild);
   // loadMore.style.display = "block";
   } catch (error) {
-    Notify.failure(error.message)
+    Notify.failure(error.message);
   }  
 }
 
@@ -102,8 +108,15 @@ function createMarkupGallery(arr) {
   }).join("");
   gallery.insertAdjacentHTML("beforeend", markup);
   lightbox.refresh();
+
+  const { height: cardHeight } = gallery.firstElementChild.getBoundingClientRect();
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: "smooth",
+  });
 }
 
+// Інфініті скрол========================================
 async function onIntersection(entries) {
   if (entries[0].isIntersecting) {
     try {
@@ -122,17 +135,17 @@ async function onIntersection(entries) {
 }
 
 
-// Кнопка-функція.Завантажити ще заображення=====================================
-// async function loadMoreImages() {
-//   try {
-//      const response = await getImages();
-//      createMarkupGallery(response.hits);
-//      if (response.totalHits <= limit * page) {
-//      loadMore.style.display = "none";
-//      return Notify.warning("We're sorry, but you've reached the end of search results.")
-//      }     
-//     page += 1;    
-//   } catch (error) {
-//     Notify.failure(error.message)
-//   } 
-// }
+// Функція "Завантажити ще заображення"=====================================
+async function loadMoreImages() {
+  try {
+     const response = await getImages();
+     createMarkupGallery(response.hits);
+     if (response.totalHits <= limit * page) {
+     loadMore.style.display = "none";
+     return Notify.warning("We're sorry, but you've reached the end of search results.")
+     }     
+    page += 1;    
+  } catch (error) {
+    Notify.failure(error.message)
+  } 
+}
